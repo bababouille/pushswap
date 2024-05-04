b634c509bd4b9d3db1f1aa042b6a61058bfe008b
bababouille <cambier.vo@gmail.com>
2024-05-04 19:59:38 +0100

    pushswapfunct

--- pushswap.c
+++ pushswap.c
@@ -12,6 +12,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct node
 {
@@ -22,26 +23,77 @@ typedef struct node
 typedef struct
 {
     struct node *head;
+    struct node *tail;
+    char name[128];
 } stack;
 
-
-// push = add element at the begining of stack
 void    push(int data, stack *A);
-// pop = remove element from stack
 void    pop(stack *A);
-void printlist(stack *A);
+void    printlist(stack *A);
+void    swap(stack *S);
+void    ss(stack *A, stack *B);
+void    pusha(stack *A, stack *B);
+void    pushb(stack *A, stack *B);
+void    rotata(stack *A);
+void    reverserotate(stack *);
+void    reverseboth(stack *A, stack B);
 
-// sa (swap a): Swap the first 2 elements at the top of stack a.
-// Do nothing if there is only one or no elements.
-// sb (swap b): Swap the first 2 elements at the top of stack b.
-// Do nothing if there is only one or no elements.
-void swap(stack *AB)
+int main()
 {
-    node *temp = AB->head->next; //temp points to node 2
-    
-    AB->head->next = temp->next; // A->head->next points to node 3
-    temp->next = AB->head; // temp->next points to node 1
-    AB->head = temp; // A->head points to null 2
+    stack *A = malloc(sizeof(stack));
+    A->head = NULL;
+    A->tail = NULL;
+    strcpy(A->name, "a");
+
+    stack *B = malloc(sizeof(stack));
+    B->head = NULL;
+    B->tail = NULL;
+    strcpy(B->name, "b");
+}
+
+void printlist(stack *AB)
+{
+    printf("----------\n");
+    node *runner = AB->head;
+    while(runner)
+    {
+        printf("%d\n", runner->data);  
+        runner = runner->next;
+    }
+    printf("----------\n");
+}
+
+void    pop(stack *S)
+{
+    node *temp = S->head;
+
+    S->head = S->head->next;
+    free(temp);
+}
+
+void    push(int data, stack *S)
+{
+    node *new = malloc(sizeof(node));
+  
+    new->data = data;
+    new->next = S->head;
+    S->head = new;
+    if(new->next == NULL)
+    {
+        S->tail = new;
+    }
+}
+
+void    pusha(stack *A, stack *B)
+{
+    push(B->head->data, A);
+    pop(B);
+}
+
+void    pushb(stack *A, stack *B)
+{
+    push(A->head->data, B);
+    pop(A);
 }
 
 void ss(stack *A, stack *B)
@@ -59,93 +111,73 @@ void ss(stack *A, stack *B)
     B->head = temp2; // A->head points to null 2
 }
 
-void    pusha(stack *A, stack *B)
+void swap(stack *S)
 {
-    push(B->head->data, A);
-    pop(B);
+    node *temp = S->head->next; // temp points to node 2
+
+    S->head->next = temp->next; // A->head->next points to node 3
+    temp->next = S->head; // temp->next points to node 1
+    S->head = temp; // A->head points to null 2
+
+    printf("s%s\n", S->name);
 }
 
-void    pushb(stack *A, stack *B)
+void rotata(stack *S)
 {
-    push(A->head->data, B);
-    pop(A);
-}
-
-
-int main()
-{
-    stack *A = malloc(sizeof(stack));
-    A->head = NULL;
+    node *temp = S->head;
     
-    stack *B = malloc(sizeof(stack));
-    B->head = NULL;
-
-    push(1, A); push(2, A); push(3, A); push(4, A); push(5, A);
-    push(9, B); push(8, B); push(7, B); push(6, B); push(5, B);
-    
-
-    // swap(A);
-    // swap(B);
-
-    // pop(A);
-
-    // pusha(A, B);
-    // pushb(A, B);
-    // ss(A, B);
-  
-    printlist(A);
-    printlist(B);
-}
-void printlist(stack *AB)
-{
-    printf("----------\n");
-    node *runner = AB->head;
-    while(runner)
+    while (temp->next != NULL)
     {
-        printf("%d\n", runner->data);  
-        runner = runner->next;
+        temp = temp->next;
     }
-    printf("----------\n");
+    temp->next = S->head;
+    temp = temp->next;
+    S->head = S->head->next; 
+    temp->next = NULL;
 }
 
-void    pop(stack *AB)
-{
-    node *temp = AB->head;
-
-    AB->head = AB->head->next;
-    free(temp);
-}
-
-void    push(int data, stack *A)
-{
-    node *new = malloc(sizeof(node));
-  
-    new->data = data;
-    new->next = A->head;
-    A->head = new;
-}
-
-// ss : sa and sb at the same time.
-
-// pa (push a): Take the first element at the top of b and put it at the top of a.
-// Do nothing if b is empty.
-
-// pb (push b): Take the first element at the top of a and put it at the top of b.
-// Do nothing if a is empty.
-
-// ra (rotate a): Shift up all elements of stack a by 1.
-// The first element becomes the last one.
-
-// rb (rotate b): Shift up all elements of stack b by 1.
-// The first element becomes the last one.
-
-// rr : ra and rb at the same time.
 
 // rra (reverse rotate a): Shift down all elements of stack a by 1.
 // The last element becomes the first one.
 
 // rrb (reverse rotate b): Shift down all elements of stack b by 1.
 // The last element becomes the first one.
+void reverserotate(stack *S)
+{
+    node *temp = S->head; // temp = 1st node 
+
+    while (temp->next->next != NULL)
+    { 
+        temp = temp->next;
+    }
+    temp->next->next = S->head; // NULL is now = to 1st node
+    S->head = temp->next; // 1st node equal to second to last node
+    temp->next = NULL; // second to last node points to NULL
+}
+
+void reverseboth(stack *A, stack B)
+{
+    node *temp = A->head; // temp = 1st node 
+
+    while (temp->next->next != NULL)
+    { 
+        temp = temp->next;
+    }
+    temp->next->next = A->head; // NULL is now = to 1st node
+    A->head = temp->next; // 1st node equal to second to last node
+    temp->next = NULL; // second to last node points to NULL
+
+    node *temp = B->head; // temp = 1st node 
+
+    while (temp->next->next != NULL)
+    { 
+        temp = temp->next;
+    }
+    temp->next->next = B->head; // NULL is now = to 1st node
+    B->head = temp->next; // 1st node equal to second to last node
+    temp->next = NULL; // second to last node points to NULL
+}
+
 
 // rrr : rra and rrb at the same time.
 
